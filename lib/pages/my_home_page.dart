8467484b7a92f0b234fb8091241bed562900fce1
briankaboyi <brianmkaboyi@gmail.com>
2024-02-07 20:10:02 +0300

    initial commit

---
+++ lib/pages/my_home_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:get/get.dart';
+
+class MyHomePage extends GetView {  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Scaffold(
+      body: Container(
+        child: Column(
+         children:[
+           Text('Nike', style: TextStyle(),),
+         ]
+        ),
+      ),
+    );
+  }
+}
